9fdaae8ca9c746baa6ef39d7d4d7f3ab2990840f
TeeMeeLehin <samemerald8@gmail.com>
2024-02-15 15:49:26 +0100

    added task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,28 +2,28 @@
 
 const request = require('request');
 
-const movie_id = process.argv[2];
+const movieId = process.argv[2];
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, (err, response) => {
-    if (err) {
-        console.log(err);
-    } else if (response.statusCode === 200) {
-        const charctrs = JSON.parse(response.body).characters;
-        for (const charctr of charctrs) {
-            request(charctr, (err, response) => {
-                if (err) {
-                    console.log(err);
-                } else if (response.statusCode === 200) {
-                    const name = JSON.parse(response.body).name;
-                    console.log(name);
-                } else {
-                    console.log(response.statusCode);
-                }
-            })
-        };
-    } else {
-        console.log(response.statusCode);
-    }
-})
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const charctrs = JSON.parse(response.body).characters;
+    charctrs.forEach(charctr => {
+      request(charctr, (err, response) => {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          const name = JSON.parse(response.body).name;
+          console.log(name);
+        } else {
+          console.log(response.statusCode);
+        }
+      });
+    });
+  } else {
+    console.log(response.statusCode);
+  }
+});
