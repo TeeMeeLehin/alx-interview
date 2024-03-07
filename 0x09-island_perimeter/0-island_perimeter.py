ea388c67cd3b7cd7446b41d79207ce4825dcaf71
TeeMeeLehin <samemerald8@gmail.com>
2024-03-07 20:50:33 +0100

    added code files

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,7 +4,7 @@ that returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
-    """func def"""
+    """func definition"""
     if not grid:
         return 0
 
@@ -14,12 +14,11 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Assume all sides are land
+                perimeter += 4
 
-                # Check neighbors
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if neighbor to the top is land
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if neighbor to the left is land
+                    perimeter -= 2
 
     return perimeter
